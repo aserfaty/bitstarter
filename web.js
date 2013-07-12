70c6478d9228385db4db590cd2ac60ebbaea8a2d
Armand Serfaty <aserfaty@verizon.net>
2013-07-12 03:24:03 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
+
+var data = fs.readFileSync('index.html');
+var buf = new Buffer(data);
+var filecontent = buf.toString();
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(filecontent);
 });
 
 var port = process.env.PORT || 5000;
